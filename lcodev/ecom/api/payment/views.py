58ef6382ee65da2716528d245f74b9cde2621dae
salonidarji <salonidarji@gmail.com>
2020-08-27 19:24:45 +0530

    backend payment done

--- lcodev/ecom/api/payment/views.py
+++ lcodev/ecom/api/payment/views.py
@@ -1,3 +1,57 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, JsonResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user_model
+from django.views.decorators.csrf import csrf_exempt
+import braintree
 # Create your views here.
+
+gateway = braintree.BraintreeGateway(
+    braintree.Configuration(
+        braintree.Environment.Sandbox,
+        merchant_id="zgxjpx2mrkvdpvcq",
+        public_key="9gvcfj66n4q894rs",
+        private_key="c08366e02c200ea9eac0d5921a30de81"
+    )
+)
+
+
+def validate_user_session(id, token):
+    UserModel = get_user_model()
+    try:
+        user = UserModel.objects.get(pk=id)
+        if user.session_token == token:
+            return True
+        return False
+    except UserModel.DoesNotExist:
+        return False
+
+
+@csrf_exempt
+def generate_token(request, id, token):
+    if not validate_user_session(id, token):
+        return JsonResponse({'error': 'Invalid session. Login again!'})
+
+    return JsonResponse({'client_token': gateway.client_token.generate(), 'Success': True})
+
+
+@csrf_exempt
+def process_payment(request, id, token):
+    if not validate_user_session(id, token):
+        return JsonResponse({'error': 'Invalid session. Login again!'})
+
+    nounce_from_client = request.POST["paymentMethodNounce"]
+    amount_from_client = request.POST["amount"]
+
+    result = gateway.transaction.sale({
+        "amount": amount_from_client,
+        "payment_method_nonce": nonce_from_the_client,
+        "options": {
+            "submit_for_settlement": True
+        }
+    })
+
+    if result.is_success:
+        return JsonResponse({'Success': result.is_success, 'transaction': {'id': result.transaction.id, 'amount': result.transaction.amount}})
+    else:
+        return JsonResponse({'error': True, 'Success': False})
